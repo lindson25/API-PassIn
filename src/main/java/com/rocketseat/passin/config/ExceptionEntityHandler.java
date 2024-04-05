3cbbd8a53616f9cb0cdebf527769aef9d7f1cb9f
Líndson Souza <146979305+lindson25@users.noreply.github.com>
2024-04-05 12:05:31 -0300

    Project

---
+++ src/main/java/com/rocketseat/passin/config/ExceptionEntityHandler.java
@@ -0,0 +1,41 @@
+package com.rocketseat.passin.config;
+
+import com.rocketseat.passin.domain.attendee.exceptions.AttendeeAlreadyExistsException;
+import com.rocketseat.passin.domain.attendee.exceptions.AttendeeNotFoundException;
+import com.rocketseat.passin.domain.checkin.exceptions.CheckInAlreadyExistsException;
+import com.rocketseat.passin.domain.event.exceptions.EventFullException;
+import com.rocketseat.passin.domain.event.exceptions.EventNotFoundException;
+import com.rocketseat.passin.dto.general.ErrorResponseDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionEntityHandler {
+
+    @ExceptionHandler(EventNotFoundException.class)
+    public ResponseEntity handleEventNotFound(EventNotFoundException exception) {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(AttendeeNotFoundException.class)
+    public ResponseEntity handleAttendeeNotFound(AttendeeNotFoundException exception) {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(AttendeeAlreadyExistsException.class)
+    public ResponseEntity handleAttendeeAlreadyExists(AttendeeAlreadyExistsException exception) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).build();
+    }
+
+    @ExceptionHandler(CheckInAlreadyExistsException.class)
+    public ResponseEntity handleCheckInAlreadyExists(CheckInAlreadyExistsException exception) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).build();
+    }
+
+    @ExceptionHandler(EventFullException.class)
+    public ResponseEntity<ErrorResponseDTO> handleEventFull(EventFullException exception) {
+        return ResponseEntity.badRequest().body(new ErrorResponseDTO(exception.getMessage()));
+    }
+}
---
+++ src/main/java/com/rocketseat/passin/domain/attendee/exceptions/AttendeeAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.rocketseat.passin.domain.attendee.exceptions;
+
+public class AttendeeAlreadyExistsException extends RuntimeException {
+    public AttendeeAlreadyExistsException(String message) {
+        super(message);
+    }
+}
